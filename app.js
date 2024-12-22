52c03ab3c0be1514c41256f1996e14e06db9c713
Marcioprado123 <Marciopiza477@gmail.com>
2024-12-22 20:52:08 -0300

    Primeiro commit - página interativa

---
+++ app.js
@@ -0,0 +1,9 @@
+let nota = 10;
+
+if (nota >= 9) {
+    console.log("Você tirou A, boa filhote");
+} else if (nota >= 7) {
+    console.log("Você tirou B, foi bom até");
+} else { 
+    console.log("Você tirou C, melhore");
+}
